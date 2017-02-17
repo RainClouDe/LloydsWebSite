fba054a4abb9f969a5e193f6ebaa319a8c88b85e
ivanw <ivanw@DESKTOP-0UHTVIM>
2017-02-17 13:10:04 -0800

    Initialising Project by adding the serivce

---
+++ Lweb/Lweb/HelloWorld.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lweb
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
+            ServiceReference1.ID_Table[] detailslist;
+
+            detailslist = proxy.GetUserDetails();
+
+            for (int i = 0; i < detailslist.Length; i++)
+            {
+                testdiv.InnerHtml = detailslist[i].Name;
+                testdiv.InnerHtml += detailslist[i].Surname;
+            }
+        }
+    }
+}
